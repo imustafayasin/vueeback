37e2002e8f5b35c3aca1ac30814e788a4dd1e3b6
Mustafa Yasin <76474748+imustafayasin@users.noreply.github.com>
2022-05-24 14:36:51 +0300

    Update handleDbErrors.js
    Remove unnecessary if condition

--- server/middleware/handleDbErrors.js
+++ server/middleware/handleDbErrors.js
@@ -1,11 +1,11 @@
 function handleErrors(errors) {
     if (!errors) return;
     let error_messages = "";
-    if (errors) {
-        Object.values(errors).forEach(er => {
-            error_messages += er?.properties?.message + '\n';
-        });
-    }
+    
+    Object.values(errors).forEach(er => {
+        error_messages += er?.properties?.message + '\n';
+    });
+    
     return error_messages
 }
 
