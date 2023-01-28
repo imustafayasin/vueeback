ea4f41a6524bec1ac4b6235d961a33477e8d7534
imustafayasin <imustafayasin@gmail.com>
2023-01-28 13:27:32 +0300

    Fix path

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ const includeUserID = (req, res, next) => {
 app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
+    res.sendFile(path.join(__dirname, './dist/index.html'))
 })
 app.get('*', (req, res) => {
     res.redirect('/')
