eb200177b6890dcdf2248e4b4fe58d813853fd8a
imustafayasin <imustafayasin@gmail.com>
2023-01-28 13:35:26 +0300

    Fix static express

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const includeUserID = (req, res, next) => {
     })
 }
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'))
